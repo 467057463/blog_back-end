5d7fedf79a5cdc80c3781a899cbbe43d6ee76c57
m2 <467057463@qq.com>
2024-03-02 21:16:07 +0800

    feat:
    配置数据库及自动化部署

--- config/config.local.js
+++ config/config.local.js
@@ -6,7 +6,7 @@ module.exports = appInfo => {
       password: 'nandudu_',
       host: '127.0.0.1',
       port: 3306,
-      database: 'learning_development',
+      database: 'blog_development',
     },
   };
 };
