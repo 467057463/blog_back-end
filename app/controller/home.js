4f2bab33389b5fabf5a9110fbf733fb2d8034220
m2 <467057463@qq.com>
2024-03-02 17:10:12 +0800

    feat:
    1. init

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,6 +3,7 @@ const { Controller } = require('egg');
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
+    console.log(this.app.config);
     ctx.body = 'hi, egg';
   }
 }
--- config/config.default.js
+++ config/config.default.js
@@ -8,7 +8,13 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = exports = {
+    security: {
+      csrf: {
+        enable: false,
+      },
+    },
+  };
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1706712360203_3983';
@@ -19,6 +25,14 @@ module.exports = appInfo => {
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
+    sequelize: {
+      dialect: 'mysql',
+      password: 'nandudu_',
+      host: '127.0.0.1',
+      port: 3306,
+      database: 'learning_development',
+    },
+    databasename: 'test',
   };
 
   return {
